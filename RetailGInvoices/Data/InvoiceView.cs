bf0017cb16f58fd008e79067a5010cc0535b02e1
darrenowen <darren.owen2@wickes.co.uk>
2020-07-21 18:48:46 +0100

    Got notes modal working

--- RetailGInvoices/Data/InvoiceView.cs
+++ RetailGInvoices/Data/InvoiceView.cs
@@ -31,6 +31,6 @@ namespace RetailGInvoices.Data
         public int InvoiceSupplierId { get; set; }
         public int InvoiceSiteId { get; set; }
         public string InvoiceNotes { get; set; }
-        public string RowClass => (InvoiceNotes == null) ? null: "table-danger";
+        public string RowClass => (InvoiceNotes == null) ? null: "table-info";
     }
 }
